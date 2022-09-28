15e102ed4ecf0159b9185054da0379d3f05d4514
Siddharth Joshi <siddharth30081@gmail.com>
2022-09-28 22:06:39 +0530

    Database Creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('./db/db')
 const express=require('express')
 const app=express()
 const port=3000
---
+++ src/secrets.js
@@ -0,0 +1,8 @@
+const database={
+    DATABASE_NAME:"bjmapvfemg92dfc2aayl",
+    DATABASE_USERNAME:"uisoyts6gxjkwpsz",
+    DATABASE_PASSWORD:"qb7d2Do3GdOQgaiC90OG",
+    DATABASE_HOST:"bjmapvfemg92dfc2aayl-mysql.services.clever-cloud.com",
+    DATABASE_DIALECT:"mysql"
+}
+module.exports=database;
